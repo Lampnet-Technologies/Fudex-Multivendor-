020be7b7487fd8ea71e75b20720c491d94e2b303
David Arinze <fivelanes72@gmail.com>
2024-05-25 11:29:19 +0100

    Designed the LandingPageHeader component and imported it in the landingpage

--- src/Pages/UserAccount/TransactionHistory/AllTransactionHistory.jsx
+++ src/Pages/UserAccount/TransactionHistory/AllTransactionHistory.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
-import { Container, Typography, Box } from "@mui/material";
+import { Container, Typography, Box, Button } from "@mui/material";
 import ChevronIcon from "../../../components/ChevronIconn/ChevronIcon";
 
 
@@ -16,21 +16,48 @@ const AllTransactionHistory = () => {
         
         {/* Top Section */}
         <Box
-        sx={{
-        marginTop: "4rem",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "1rem",
-        cursor: "pointer",
-        width: "100%",
-        maxWidth: "1050px",
-        }}
-        onClick={handleChevronClick}
-    >
-        <ChevronIcon size='2rem' />
-        <Typography variant='h6'>Profile Details</Typography>
-    </Box>
+            sx={{
+            marginTop: "4rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "1rem",
+            cursor: "pointer",
+            width: "100%",
+            maxWidth: "1050px",
+            }}
+            onClick={handleChevronClick}
+            >
+            <ChevronIcon size='2rem' />
+            <Typography variant='h6'>Profile Details</Typography>
+       
+
+             {/* wallet balance Section */}
+             <Box sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'row',
+                gap: '2rem',
+                
+                }}>
+                <Typography variant='h6' >Wallet Balance</Typography>
+                <Button
+                variant='contained'
+                type='submit'
+                sx={{
+                backgroundColor: "#F6613F",
+                height: "45px",
+                
+                "&:hover": {
+                    backgroundColor: "#D95436",
+                },
+                }}
+                >
+                Fund Wallet
+                </Button>
+            </Box>
+        </Box>
    </Container>
   )
 }
